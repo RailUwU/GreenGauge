fc68c1c588504d691873e262df3a6bec94fc8949
RailUwU <134000320+RailUwU@users.noreply.github.com.>
2025-01-24 09:00:36 +0530

    Deleted unnecessary node files

--- GreenGauge/GreenGauge/urls.py
+++ GreenGauge/GreenGauge/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),  # Include home app URLs
+    path('login/', views.login_view, name='login'), #Login URL
+    path('signup/', views.signup_view, name='signup'), #Signup URL
 ]
--- GreenGauge/home/urls.py
+++ GreenGauge/home/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home_dashboard, name='home_dashboard'),  # Root URL
     path('dashboard/', views.home_dashboard, name='home_dashboard'),  # Dashboard URL
-    path('login/', views.login_view, name='login'), #Login URL
-    path('signup/', views.signup_view, name='signup'), #Login URL
+    
 ]
