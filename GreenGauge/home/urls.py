89a9cd069d348dc56bf1ec187f72e69ea305f6a0
RailUwU <134000320+RailUwU@users.noreply.github.com.>
2025-01-24 07:21:36 +0530

    Homepage now directs to login and signup

--- GreenGauge/home/urls.py
+++ GreenGauge/home/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home_dashboard, name='home_dashboard'),  # Root URL
     path('dashboard/', views.home_dashboard, name='home_dashboard'),  # Dashboard URL
+    path('login/', views.login_view, name='login'), #Login URL
+    path('signup/', views.signup_view, name='signup'), #Login URL
 ]
--- GreenGauge/home/views.py
+++ GreenGauge/home/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home_dashboard(request):
     return render(request, 'home/dashboard.html')
+
+def login_view(request):
+    return render(request,'login.html')
+
+def signup_view(request):
+    return render(request,'signup.html')
